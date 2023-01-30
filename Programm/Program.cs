c5f036561bd8d992a37c75ea63f1fb0fc02f0422
NY <oleg.kostyukof@yandex.ru>
2023-01-30 14:04:49 +0300

    первый коммит

--- Programm/Program.cs
+++ Programm/Program.cs
@@ -6,50 +6,6 @@
 
 
 
-// Создание массива строк
-static string[] CreateArray(int args)
-    {
-        return new string[args];
-    }
-
-// Печать массива строк
-    static void PrintArray(string[] array)
-    {
-
-        int size = array.Length;
-        for (int i = 0; i < size; i++)
-        {
-            Console.Write(array[i]);
-            
-            
-        }
-
-    }
-
-
-
-
-string [] M = {"rsat", "ls", "1", "112", "1л11"};
-string [] N = CreateArray (M.Length);
-
-int index = 0;
-
-for (int i = 0; i < M.Length; i++)
-{
-if (M[i].Length <= 3)
-
-
-
-
-N[index] = M[i];
-
-
-
-
-}
-
-
-PrintArray (N);
 
 
 
