f4d8abe84c2dfd9e21bffa3cdf1be189023b5307
NY <oleg.kostyukof@yandex.ru>
2023-01-30 14:19:57 +0300

    Create_metod Print Array

--- Programm/Program.cs
+++ Programm/Program.cs
@@ -16,6 +16,19 @@ static string[] CreateArray(int args)
 
 
 
+// Печать массива строк
+    static void PrintArray(string[] array)
+    {
+
+        int size = array.Length;
+        for (int i = 0; i < size; i++)
+        {
+            Console.Write(array[i]);
+            
+            
+        }
+
+    }
 
 
 
